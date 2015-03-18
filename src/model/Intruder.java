cb6952615a210aeed41d7417c223c7f3bd923512
Robert Stevens <robert@stevens.tk>
2015-03-18 20:57:54 +0100

    2 bugfixes

--- src/model/Intruder.java
+++ src/model/Intruder.java
@@ -1,5 +1,12 @@
 package model;
 
-public class Intruder {
+import model.commands.Coordinates;
+
+public class Intruder extends Agent{
+
+	public Intruder(Coordinates coord) {
+		super(coord);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/model/Sentry.java
+++ src/model/Sentry.java
@@ -1,7 +1,14 @@
 package model;
 
+import model.commands.Coordinates;
+
 
 public class Sentry extends Guard{
+
+	public Sentry(Coordinates coord) {
+		super(coord);
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 
