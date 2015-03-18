31466d82ffe65ae5da5396b93a901e7f30fa329c
Robert Stevens <robert@stevens.tk>
2015-03-18 22:17:29 +0100

    added an angle to the coordinates

--- src/model/commands/Coordinates.java
+++ src/model/commands/Coordinates.java
@@ -5,10 +5,21 @@ public class Coordinates {
 	{
 		setCoordinates(x, y);
 	}
+	
+	public Coordinates(int x, int y, double angle)
+	{
+		setCoordinates(x, y, angle);
+	}
+
 	public void setCoordinates(int x, int y)
+	{
+		setCoordinates(x, y, 0);
+	}
+	public void setCoordinates(int x, int y, double angle)
 	{
 		this.x = x;
 		this.y = y;
+		this.angle = angle;
 	}
 	public int getX(){
 		return this.x;
@@ -17,6 +28,12 @@ public class Coordinates {
 		return this.y;
 	}
 	
+	public double getAngle()
+	{
+		return this.angle;
+	}
+	
 	private int x;
 	private int y;
+	private double angle;
 }
