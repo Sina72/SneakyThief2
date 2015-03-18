7150509a59580647d62fc13a9c0a862dc7758093
Robert Stevens <robert@stevens.tk>
2015-03-18 21:48:34 +0100

    added a new class that should update evry step

--- src/controller/GameMain.java
+++ src/controller/GameMain.java
@@ -5,10 +5,16 @@ import model.*;
 public class GameMain {
 
 	public static void main(String[] args) {
+		/* initialise the systems */ 
 		Map map = new MapBuilder().buildDefaultMap();
 		if (map == null)
 			new Exception("the map is not generated correctly");
 		AgentIndex agentIndex = new AgentIndex(map);
+		
+		/* add all initialistations before this point */
+		StepUpdater updater = new StepUpdater(0.1);
+		
+		
 		// TODO Auto-generated method stub
 
 	}
---
+++ src/controller/StepUpdater.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class StepUpdater {
+	public StepUpdater(double stepSize)
+	{
+		m_stepSize = stepSize;
+	}
+	
+	public void NextStep()
+	{
+	//TODO: add evrything here that has to be updated
+	}
+	private double m_stepSize;
+	private int m_stepNumber;
+}
