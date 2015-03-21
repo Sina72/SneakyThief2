ea84ba630c61c237c8cfc56337bc4f712fc82c47
Stan Kerstjens <s.kerstjens@student.maastrichtuniversity.nl>
2015-03-21 11:03:44 +0100

    Meter to gridstates and back conversion methods Constructor for map in meters

--- src/model/Map.java
+++ src/model/Map.java
@@ -8,26 +8,100 @@ import model.commands.*;
  */
 public class Map {
 	
+	/* Constants */
+	private static final int GRIDSTATES_PER_METER = 1;
+	
+	
+	/*		Constructors	 */
 	/**
-	 * Create a map initualise it with empty spots, except from the border this is initalised with the outerwall
+	 * Constructor taking an amount of meters.
+	 * @param width the width of the map in meters
+	 * @param height of the map in meters
+	 */
+	public Map(double width, double height){
+		this(	metersToGridStates(width),
+				metersToGridStates(height));
+	}
+	
+	/**
+	 * Create a map initualise it with empty spots, except from the border this is initalised with the outerwall. 
+	 * Constructor takes an amount of gridstates
 	 * @param width the width of the map
 	 * @param height the height of the map
 	 */
 	public Map (int width, int height)
 	{
-		theMap = new GridState[width][height];
-		for (int i = 0; i < theMap.length; i++)
-			for (int j = 0; j <theMap[0].length; j++)
-				if (i == 0 || i == theMap.length-1 || j == 0 || j == theMap[0].length-1)
+		m_width = width;
+		m_height = height;
+		
+		/*	initialize map with outer wall 	*/
+		theMap = new GridState[m_width][m_height];
+		for (int i = 0; i < m_height; i++)
+			for (int j = 0; j <m_width; j++)
+				if (i == 0 || i == m_height-1 || j == 0 || j == m_width-1)
 					theMap[i][j] = GridState.OuterWall;
 				else
 					theMap[i][j] = GridState.Empty;
 	}
 	
+	/*	 Conversion Methods		*/
+	
+	/**
+	 * Convenience method for converting any amount of meter values to an array of gridState amounts.
+	 * Calls the metersToGridstates method on every value
+	 * @param meters
+	 * @return integer array of amount of gridStates per meters in the same order as offered.
+	 */
+	public static int[] metersToGridstates(double... meters){
+		int[] gridStates = new int[meters.length];
+		for(int i = 0; i < gridStates.length; i ++)
+			gridStates[i] = metersToGridStates(meters[i]);
+		return gridStates;
+	}
+	
+	/**
+	 * Converts an amount of meters to an amount of gridStates. Amount of gridStates per meter is {@value #GRIDSTATES_PER_METER}.
+	 * 
+	 * This method will round down on the amount of gridStates, e.g. 2.99 gridStates will be truncated to 2 gridStates
+	 * @param meters
+	 * @return
+	 */
+	public static int metersToGridStates(double meters){
+		return (int) (meters * GRIDSTATES_PER_METER);
+	}
+	
+	/**
+	 * Convenience method for converting any amount of gridState amounts to an array of meter values
+	 * @param Gridstates amount of gridStates
+	 * @return double array of meter values in the order as offered
+	 */
+	public static double[] gridstatesToMeters(int... gridStates){
+		double[] meters = new double[gridStates.length];
+		for(int i = 0; i < meters.length; i ++)
+			meters[i] = gridStatesToMeters(gridStates[i]);
+		return meters;
+	}
+	
+	/**
+	 * Converts an amount of gridStates to a meter value. Although the meter is a double, one should pay attention that the amount of
+	 * gridStates is discretised.
+	 * @param gridstates an amount of gridStates
+	 * @return
+	 */
+	public static double gridStatesToMeters(int gridstates){
+		return gridstates / GRIDSTATES_PER_METER;
+	}
+	
+	/*	Getters/Setters */
+	
 	public GridState[][] getMap(){
 		return theMap;
 	}
 	
+	/*	Instance Variables	*/
 	private GridState[][] theMap;
+	//Width in amount of gridstates
+	private int m_width;
+	private int m_height;
 
 }
